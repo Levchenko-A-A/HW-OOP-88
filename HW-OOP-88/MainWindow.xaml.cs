1228c8caee396d7cf9a2622e96e8493b717b8310
Андрей Левченков <lev_a_a@mail.ru>
2025-03-05 23:31:11 +0200

    Добавьте файлы проекта.

---
+++ HW-OOP-88/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW_OOP_88;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    DbPersonContext db = new DbPersonContext();
+    public Person Person;
+    public MainWindow()
+    {
+        InitializeComponent();
+        db.Persons.Load();
+        
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        Person = db.Persons.First()!;
+        DataContext = Person;
+    }
+}
---
+++ HW-OOP-88/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace HW_OOP_88;
+
+public partial class Person: INotifyPropertyChanged
+{
+    private int id;
+    public int Id 
+    {
+        get => id; 
+        set
+        {
+            id = value;
+            OnPropertyChanged("Id");
+        }
+    }
+
+    private string name;
+    public string Name 
+    {
+        get => name; 
+        set
+        {
+            name = value;
+            OnPropertyChanged("Name");
+        }
+    }
+
+    private int age;
+    public int Age 
+    {
+        get => age; 
+        set
+        {
+            age = value;
+            OnPropertyChanged("Age");
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
